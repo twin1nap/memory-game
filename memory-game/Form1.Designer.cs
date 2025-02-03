8a22e0f4b03e90b0e5bd6c2312bc8d32765e5a58
Joran Nap <143212549+twin1nap@users.noreply.github.com>
2025-02-03 15:39:17 +0100

    some features toegevoegd

--- memory-game/Form1.Designer.cs
+++ memory-game/Form1.Designer.cs
@@ -28,69 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn4
             // 
-            this.button1.Location = new System.Drawing.Point(55, 56);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn4.FlatAppearance.BorderSize = 0;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Image = global::memory_game.Properties.Resources.HondFoto2;
+            this.btn4.Location = new System.Drawing.Point(286, 263);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(175, 175);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
             // 
-            // button2
+            // btn3
             // 
-            this.button2.Location = new System.Drawing.Point(175, 55);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn3.FlatAppearance.BorderSize = 0;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Image = global::memory_game.Properties.Resources.HondFoto2;
+            this.btn3.Location = new System.Drawing.Point(55, 263);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(175, 175);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
             // 
-            // button3
+            // btn2
             // 
-            this.button3.Location = new System.Drawing.Point(55, 98);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Image = global::memory_game.Properties.Resources.HondFoto1;
+            this.btn2.Location = new System.Drawing.Point(286, 56);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(175, 175);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
             // 
-            // button4
+            // btn1
             // 
-            this.button4.Location = new System.Drawing.Point(175, 98);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Image = global::memory_game.Properties.Resources.HondFoto1;
+            this.btn1.Location = new System.Drawing.Point(55, 56);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(175, 175);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(620, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- memory-game/Form1.cs
+++ memory-game/Form1.cs
@@ -12,9 +12,37 @@ namespace memory_game
 {
     public partial class Form1 : Form
     {
+        int btn_count = 0;
+        Button[] clicked_buttons = new Button[2];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            button.FlatAppearance.BorderColor = Color.Red;
+
+            if (btn_count < 2)
+            {
+                btn_count++;
+                button.FlatAppearance.BorderSize = 3;
+            }
+
+            clicked_buttons.Append(button);
+            if (btn_count == 2)
+            {
+                if (clicked_buttons[0].Image == clicked_buttons[1].Image)
+                {
+                    label1.Text = "ze zijn hetzelfde";
+                }
+                else
+                {
+                    label1.Text = "ze zijn niet hetzelfde";
+                }
+            }
+
         }
     }
 }
