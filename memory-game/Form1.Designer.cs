48bb8ba8e9bd376745ff6930ff8867b203c12fc0
Joran Nap <143212549+twin1nap@users.noreply.github.com>
2025-02-05 13:50:30 +0100

    randomize + controle

--- memory-game/Form1.Designer.cs
+++ memory-game/Form1.Designer.cs
@@ -28,64 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn4 = new System.Windows.Forms.Button();
-            this.btn3 = new System.Windows.Forms.Button();
-            this.btn2 = new System.Windows.Forms.Button();
-            this.btn1 = new System.Windows.Forms.Button();
+            this.card4 = new System.Windows.Forms.Button();
+            this.card3 = new System.Windows.Forms.Button();
+            this.card2 = new System.Windows.Forms.Button();
+            this.card1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn4
+            // card4
             // 
-            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Red;
-            this.btn4.FlatAppearance.BorderSize = 0;
-            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn4.Image = global::memory_game.Properties.Resources.HondFoto2;
-            this.btn4.Location = new System.Drawing.Point(286, 263);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(175, 175);
-            this.btn4.TabIndex = 3;
-            this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            this.card4.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.card4.FlatAppearance.BorderSize = 0;
+            this.card4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.card4.Image = global::memory_game.Properties.Resources.HondFoto2;
+            this.card4.Location = new System.Drawing.Point(286, 263);
+            this.card4.Name = "card4";
+            this.card4.Size = new System.Drawing.Size(175, 175);
+            this.card4.TabIndex = 3;
+            this.card4.Tag = "2";
+            this.card4.UseVisualStyleBackColor = true;
+            this.card4.Click += new System.EventHandler(this.btn_Click);
             // 
-            // btn3
+            // card3
             // 
-            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
-            this.btn3.FlatAppearance.BorderSize = 0;
-            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn3.Image = global::memory_game.Properties.Resources.HondFoto2;
-            this.btn3.Location = new System.Drawing.Point(55, 263);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(175, 175);
-            this.btn3.TabIndex = 2;
-            this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            this.card3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.card3.FlatAppearance.BorderSize = 0;
+            this.card3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.card3.Image = global::memory_game.Properties.Resources.HondFoto2;
+            this.card3.Location = new System.Drawing.Point(55, 263);
+            this.card3.Name = "card3";
+            this.card3.Size = new System.Drawing.Size(175, 175);
+            this.card3.TabIndex = 2;
+            this.card3.Tag = "2";
+            this.card3.UseVisualStyleBackColor = true;
+            this.card3.Click += new System.EventHandler(this.btn_Click);
             // 
-            // btn2
+            // card2
             // 
-            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
-            this.btn2.FlatAppearance.BorderSize = 0;
-            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn2.Image = global::memory_game.Properties.Resources.HondFoto1;
-            this.btn2.Location = new System.Drawing.Point(286, 56);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(175, 175);
-            this.btn2.TabIndex = 1;
-            this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            this.card2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.card2.FlatAppearance.BorderSize = 0;
+            this.card2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.card2.Image = global::memory_game.Properties.Resources.HondFoto1;
+            this.card2.Location = new System.Drawing.Point(286, 65);
+            this.card2.Name = "card2";
+            this.card2.Size = new System.Drawing.Size(175, 175);
+            this.card2.TabIndex = 1;
+            this.card2.Tag = "1";
+            this.card2.UseVisualStyleBackColor = true;
+            this.card2.Click += new System.EventHandler(this.btn_Click);
             // 
-            // btn1
+            // card1
             // 
-            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
-            this.btn1.FlatAppearance.BorderSize = 0;
-            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn1.Image = global::memory_game.Properties.Resources.HondFoto1;
-            this.btn1.Location = new System.Drawing.Point(55, 56);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(175, 175);
-            this.btn1.TabIndex = 0;
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            this.card1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.card1.FlatAppearance.BorderSize = 0;
+            this.card1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.card1.Image = global::memory_game.Properties.Resources.HondFoto1;
+            this.card1.Location = new System.Drawing.Point(55, 56);
+            this.card1.Name = "card1";
+            this.card1.Size = new System.Drawing.Size(175, 175);
+            this.card1.TabIndex = 0;
+            this.card1.Tag = "1";
+            this.card1.UseVisualStyleBackColor = true;
+            this.card1.Click += new System.EventHandler(this.btn_Click);
             // 
             // label1
             // 
@@ -95,18 +100,30 @@
             this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 4;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(628, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btn4);
-            this.Controls.Add(this.btn3);
-            this.Controls.Add(this.btn2);
-            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.card4);
+            this.Controls.Add(this.card3);
+            this.Controls.Add(this.card2);
+            this.Controls.Add(this.card1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,11 +131,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btn1;
-        private System.Windows.Forms.Button btn2;
-        private System.Windows.Forms.Button btn3;
-        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button card1;
+        private System.Windows.Forms.Button card2;
+        private System.Windows.Forms.Button card3;
+        private System.Windows.Forms.Button card4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- memory-game/Form1.cs
+++ memory-game/Form1.cs
@@ -13,27 +13,82 @@ namespace memory_game
     public partial class Form1 : Form
     {
         int btn_count = 0;
-        Button[] clicked_buttons = new Button[2];
+        string[] clicked_buttons = new string[2];
+        Image[] card_img = new Image[4]
+        {
+            Properties.Resources.HondFoto1,
+            Properties.Resources.HondFoto1,
+            Properties.Resources.HondFoto2,
+            Properties.Resources.HondFoto2
+        };
+        string[] card_tags = new string[4]
+        {
+            "1",
+            "1",
+            "2",
+            "2"
+        };
+
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //asign images to buttons
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //shuffle cards using fisher-yates shuffle (learned from chatgpt creatued by typing for and tab and that made whole code)
+            for (int i = card_img.Length - 1; i > 0; i--)
+            {
+                int j = new Random().Next(i + 1);
+
+                Image temp = card_img[i];
+                card_img[i] = card_img[j];
+                card_img[j] = temp;
+
+                string temp_tag = card_tags[i];
+                card_tags[i] = card_tags[j];
+                card_tags[j] = temp_tag;
+            }
+
+            card1.Image = card_img[0];
+            card1.Tag = card_tags[0];
+            card2.Image = card_img[1];
+            card2.Tag = card_tags[1];
+            card3.Image = card_img[2];
+            card3.Tag = card_tags[2];
+            card4.Image = card_img[3];
+            card4.Tag = card_tags[3];
+
+
+        }
+
+
+        //compaire cards
         private void btn_Click(object sender, EventArgs e)
         {
             Button button = sender as Button;
-            button.FlatAppearance.BorderColor = Color.Red;
 
             if (btn_count < 2)
             {
+                button.FlatAppearance.BorderColor = Color.Red;
                 btn_count++;
                 button.FlatAppearance.BorderSize = 3;
             }
 
-            clicked_buttons.Append(button);
-            if (btn_count == 2)
+            if (btn_count == 1)
             {
-                if (clicked_buttons[0].Image == clicked_buttons[1].Image)
+                clicked_buttons[0] = button.Tag.ToString();
+            }
+            else if (btn_count == 2)
+            {
+                btn_count = 3;//zorgt er voor dat er niets gebeurt bij de derde klik
+                clicked_buttons[1] = button.Tag.ToString();
+                //zelfde detectie
+                if (clicked_buttons[0] == clicked_buttons[1])
                 {
                     label1.Text = "ze zijn hetzelfde";
                 }
