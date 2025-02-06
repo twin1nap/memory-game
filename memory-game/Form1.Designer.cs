451775008aec1b1ef9451ba04ba7cc83c8b66233
Joran Nap <143212549+twin1nap@users.noreply.github.com>
2025-02-06 14:38:01 +0100

    timer + form properties

--- memory-game/Form1.Designer.cs
+++ memory-game/Form1.Designer.cs
@@ -56,12 +56,9 @@
             // 
             this.card1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card1.FlatAppearance.BorderSize = 0;
-            this.card1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card1.Image = ((System.Drawing.Image)(resources.GetObject("card1.Image")));
-            this.card1.Location = new System.Drawing.Point(205, 24);
+            resources.ApplyResources(this.card1, "card1");
+            this.card1.Image = global::memory_game.Properties.Resources.HondFoto1;
             this.card1.Name = "card1";
-            this.card1.Size = new System.Drawing.Size(175, 175);
-            this.card1.TabIndex = 0;
             this.card1.Tag = "1";
             this.card1.UseVisualStyleBackColor = true;
             this.card1.Click += new System.EventHandler(this.Card_Click);
@@ -70,12 +67,9 @@
             // 
             this.card2.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card2.FlatAppearance.BorderSize = 0;
-            this.card2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card2.Image = ((System.Drawing.Image)(resources.GetObject("card2.Image")));
-            this.card2.Location = new System.Drawing.Point(436, 24);
+            resources.ApplyResources(this.card2, "card2");
+            this.card2.Image = global::memory_game.Properties.Resources.HondFoto1;
             this.card2.Name = "card2";
-            this.card2.Size = new System.Drawing.Size(175, 184);
-            this.card2.TabIndex = 1;
             this.card2.Tag = "1";
             this.card2.UseVisualStyleBackColor = true;
             this.card2.Click += new System.EventHandler(this.Card_Click);
@@ -84,12 +78,8 @@
             // 
             this.card5.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card5.FlatAppearance.BorderSize = 0;
-            this.card5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card5.Image = ((System.Drawing.Image)(resources.GetObject("card5.Image")));
-            this.card5.Location = new System.Drawing.Point(205, 231);
+            resources.ApplyResources(this.card5, "card5");
             this.card5.Name = "card5";
-            this.card5.Size = new System.Drawing.Size(175, 175);
-            this.card5.TabIndex = 2;
             this.card5.Tag = "3";
             this.card5.UseVisualStyleBackColor = true;
             this.card5.Click += new System.EventHandler(this.Card_Click);
@@ -98,12 +88,8 @@
             // 
             this.card6.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card6.FlatAppearance.BorderSize = 0;
-            this.card6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card6.Image = ((System.Drawing.Image)(resources.GetObject("card6.Image")));
-            this.card6.Location = new System.Drawing.Point(436, 231);
+            resources.ApplyResources(this.card6, "card6");
             this.card6.Name = "card6";
-            this.card6.Size = new System.Drawing.Size(175, 175);
-            this.card6.TabIndex = 3;
             this.card6.Tag = "3";
             this.card6.UseVisualStyleBackColor = true;
             this.card6.Click += new System.EventHandler(this.Card_Click);
@@ -112,12 +98,8 @@
             // 
             this.card3.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card3.FlatAppearance.BorderSize = 0;
-            this.card3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card3.Image = ((System.Drawing.Image)(resources.GetObject("card3.Image")));
-            this.card3.Location = new System.Drawing.Point(666, 24);
+            resources.ApplyResources(this.card3, "card3");
             this.card3.Name = "card3";
-            this.card3.Size = new System.Drawing.Size(175, 175);
-            this.card3.TabIndex = 5;
             this.card3.Tag = "3";
             this.card3.UseVisualStyleBackColor = true;
             this.card3.Click += new System.EventHandler(this.Card_Click);
@@ -126,12 +108,8 @@
             // 
             this.card4.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card4.FlatAppearance.BorderSize = 0;
-            this.card4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card4.Image = ((System.Drawing.Image)(resources.GetObject("card4.Image")));
-            this.card4.Location = new System.Drawing.Point(897, 19);
+            resources.ApplyResources(this.card4, "card4");
             this.card4.Name = "card4";
-            this.card4.Size = new System.Drawing.Size(175, 184);
-            this.card4.TabIndex = 6;
             this.card4.Tag = "3";
             this.card4.UseVisualStyleBackColor = true;
             this.card4.Click += new System.EventHandler(this.Card_Click);
@@ -140,12 +118,8 @@
             // 
             this.card7.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card7.FlatAppearance.BorderSize = 0;
-            this.card7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card7.Image = ((System.Drawing.Image)(resources.GetObject("card7.Image")));
-            this.card7.Location = new System.Drawing.Point(666, 231);
+            resources.ApplyResources(this.card7, "card7");
             this.card7.Name = "card7";
-            this.card7.Size = new System.Drawing.Size(175, 175);
-            this.card7.TabIndex = 7;
             this.card7.Tag = "4";
             this.card7.UseVisualStyleBackColor = true;
             this.card7.Click += new System.EventHandler(this.Card_Click);
@@ -154,12 +128,8 @@
             // 
             this.card8.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card8.FlatAppearance.BorderSize = 0;
-            this.card8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card8.Image = ((System.Drawing.Image)(resources.GetObject("card8.Image")));
-            this.card8.Location = new System.Drawing.Point(897, 231);
+            resources.ApplyResources(this.card8, "card8");
             this.card8.Name = "card8";
-            this.card8.Size = new System.Drawing.Size(175, 175);
-            this.card8.TabIndex = 8;
             this.card8.Tag = "4";
             this.card8.UseVisualStyleBackColor = true;
             this.card8.Click += new System.EventHandler(this.Card_Click);
@@ -168,12 +138,8 @@
             // 
             this.card9.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card9.FlatAppearance.BorderSize = 0;
-            this.card9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card9.Image = ((System.Drawing.Image)(resources.GetObject("card9.Image")));
-            this.card9.Location = new System.Drawing.Point(205, 438);
+            resources.ApplyResources(this.card9, "card9");
             this.card9.Name = "card9";
-            this.card9.Size = new System.Drawing.Size(175, 175);
-            this.card9.TabIndex = 9;
             this.card9.Tag = "5";
             this.card9.UseVisualStyleBackColor = true;
             this.card9.Click += new System.EventHandler(this.Card_Click);
@@ -182,12 +148,8 @@
             // 
             this.card10.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card10.FlatAppearance.BorderSize = 0;
-            this.card10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card10.Image = ((System.Drawing.Image)(resources.GetObject("card10.Image")));
-            this.card10.Location = new System.Drawing.Point(436, 438);
+            resources.ApplyResources(this.card10, "card10");
             this.card10.Name = "card10";
-            this.card10.Size = new System.Drawing.Size(175, 184);
-            this.card10.TabIndex = 10;
             this.card10.Tag = "5";
             this.card10.UseVisualStyleBackColor = true;
             this.card10.Click += new System.EventHandler(this.Card_Click);
@@ -196,12 +158,8 @@
             // 
             this.card13.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card13.FlatAppearance.BorderSize = 0;
-            this.card13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card13.Image = ((System.Drawing.Image)(resources.GetObject("card13.Image")));
-            this.card13.Location = new System.Drawing.Point(205, 645);
+            resources.ApplyResources(this.card13, "card13");
             this.card13.Name = "card13";
-            this.card13.Size = new System.Drawing.Size(175, 175);
-            this.card13.TabIndex = 11;
             this.card13.Tag = "7";
             this.card13.UseVisualStyleBackColor = true;
             this.card13.Click += new System.EventHandler(this.Card_Click);
@@ -210,12 +168,8 @@
             // 
             this.card14.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card14.FlatAppearance.BorderSize = 0;
-            this.card14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card14.Image = ((System.Drawing.Image)(resources.GetObject("card14.Image")));
-            this.card14.Location = new System.Drawing.Point(436, 645);
+            resources.ApplyResources(this.card14, "card14");
             this.card14.Name = "card14";
-            this.card14.Size = new System.Drawing.Size(175, 175);
-            this.card14.TabIndex = 12;
             this.card14.Tag = "7";
             this.card14.UseVisualStyleBackColor = true;
             this.card14.Click += new System.EventHandler(this.Card_Click);
@@ -224,12 +178,8 @@
             // 
             this.card11.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card11.FlatAppearance.BorderSize = 0;
-            this.card11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card11.Image = ((System.Drawing.Image)(resources.GetObject("card11.Image")));
-            this.card11.Location = new System.Drawing.Point(666, 438);
+            resources.ApplyResources(this.card11, "card11");
             this.card11.Name = "card11";
-            this.card11.Size = new System.Drawing.Size(175, 175);
-            this.card11.TabIndex = 14;
             this.card11.Tag = "6";
             this.card11.UseVisualStyleBackColor = true;
             this.card11.Click += new System.EventHandler(this.Card_Click);
@@ -238,12 +188,8 @@
             // 
             this.card12.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card12.FlatAppearance.BorderSize = 0;
-            this.card12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card12.Image = ((System.Drawing.Image)(resources.GetObject("card12.Image")));
-            this.card12.Location = new System.Drawing.Point(897, 433);
+            resources.ApplyResources(this.card12, "card12");
             this.card12.Name = "card12";
-            this.card12.Size = new System.Drawing.Size(175, 184);
-            this.card12.TabIndex = 15;
             this.card12.Tag = "6";
             this.card12.UseVisualStyleBackColor = true;
             this.card12.Click += new System.EventHandler(this.Card_Click);
@@ -252,12 +198,8 @@
             // 
             this.card15.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card15.FlatAppearance.BorderSize = 0;
-            this.card15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card15.Image = ((System.Drawing.Image)(resources.GetObject("card15.Image")));
-            this.card15.Location = new System.Drawing.Point(666, 645);
+            resources.ApplyResources(this.card15, "card15");
             this.card15.Name = "card15";
-            this.card15.Size = new System.Drawing.Size(175, 175);
-            this.card15.TabIndex = 16;
             this.card15.Tag = "8";
             this.card15.UseVisualStyleBackColor = true;
             this.card15.Click += new System.EventHandler(this.Card_Click);
@@ -266,42 +208,26 @@
             // 
             this.card16.FlatAppearance.BorderColor = System.Drawing.Color.Red;
             this.card16.FlatAppearance.BorderSize = 0;
-            this.card16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.card16.Image = ((System.Drawing.Image)(resources.GetObject("card16.Image")));
-            this.card16.Location = new System.Drawing.Point(897, 645);
+            resources.ApplyResources(this.card16, "card16");
             this.card16.Name = "card16";
-            this.card16.Size = new System.Drawing.Size(175, 175);
-            this.card16.TabIndex = 17;
             this.card16.Tag = "8";
             this.card16.UseVisualStyleBackColor = true;
             this.card16.Click += new System.EventHandler(this.Card_Click);
             // 
             // txtTimer
             // 
-            this.txtTimer.AutoSize = true;
-            this.txtTimer.Location = new System.Drawing.Point(57, 71);
+            resources.ApplyResources(this.txtTimer, "txtTimer");
             this.txtTimer.Name = "txtTimer";
-            this.txtTimer.Size = new System.Drawing.Size(79, 16);
-            this.txtTimer.TabIndex = 18;
-            this.txtTimer.Text = "Timer: 00:00";
             // 
             // txtScore
             // 
-            this.txtScore.AutoSize = true;
-            this.txtScore.Location = new System.Drawing.Point(56, 253);
+            resources.ApplyResources(this.txtScore, "txtScore");
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(56, 16);
-            this.txtScore.TabIndex = 19;
-            this.txtScore.Text = "Score: 0";
             // 
             // txtBeurten
             // 
-            this.txtBeurten.AutoSize = true;
-            this.txtBeurten.Location = new System.Drawing.Point(59, 413);
+            resources.ApplyResources(this.txtBeurten, "txtBeurten");
             this.txtBeurten.Name = "txtBeurten";
-            this.txtBeurten.Size = new System.Drawing.Size(66, 16);
-            this.txtBeurten.TabIndex = 20;
-            this.txtBeurten.Text = "Beurten: 0";
             // 
             // timer
             // 
@@ -310,9 +236,8 @@
             // 
             // game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 914);
             this.Controls.Add(this.txtBeurten);
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.txtTimer);
@@ -332,9 +257,8 @@
             this.Controls.Add(this.card5);
             this.Controls.Add(this.card2);
             this.Controls.Add(this.card1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "game";
-            this.Text = "memory game";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
